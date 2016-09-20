28d207f485212166f663a319ecb7e36f76fcd9d0
Seth Bowen <sethmath@gmail.com>
2016-09-20 16:01:36 -0400

    Update Automated_Chicken_Coop.ino
    More declaring variables

--- Automated_Chicken_Coop.ino
+++ Automated_Chicken_Coop.ino
@@ -7,18 +7,18 @@
 byte waterLLPin = 6;       //Water Level Low
 byte waterLMPin = A7;      //Water Level Mid
 byte waterLHPin = A6;      //Water Level High
-byte waterLWLPin = 8;     //Water Level waterer Low
+byte waterLWLPin = 7;     //Water Level waterer Low
 byte waterMBtempPin = A0;   //Water Main bucket Temp
 byte watererBTtempPin = A1;   //Waterer Bucket Temp
 byte lightSensorPin = A2;   //Light Sensor for determining sun set and rising.
 byte batteryVoltagePin = A3;  //Battery Voltage 
 byte coopAirTempPin = A4;     //Air Temp in coop
-byte utiltiesAirTempPin = A5;   //Air Temp in utilties enclosure
-//-----------------Sensor Variables------------------------------------------- 
-int waterLH, waterLM, waterLL, waterLWL, waterMBtemp, watererBTtemp, lightSensor, batteryVoltage, coopAirTemp, utiltiesAirTemp;
-//-----------------Display Pins-----------------------------------------------
+byte utilitiesAirTempPin = A5;   //Air Temp in utilities enclosure
+//-----------------Sensor Variables--------------------------------------------
+int waterLH, waterLM, waterLL, waterLWL, waterMBtemp, watererBTtemp, lightSensor; 
+int batteryVoltage, coopAirTemp, utilitiesAirTemp;
+//-----------------Display Pins------------------------------------------------
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
 //d pins 2-10 A pin 0-3,6-7
 /* 
  * LCD RS pin to digital pin 12
@@ -32,15 +32,19 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
 */
-
 void setup() {  // put your setup code here, to run once:
-
-  lcd.begin(16, 2); //Initalize the LCD with 16 col and 2 rows
-  waterLH = waterLM = waterLL = waterLWL = waterMBtemp = watererBTtemp = lightSensor = batteryVoltage = coopAirTemp = utiltiesAirTemp = 0;
- // pinMode(red, OUTPUT); pinMode(yellow, OUTPUT);  pinMode(green, OUTPUT);
-  Serial.begin(19200);
-  while (!Serial)
- // flashLED(red, 6, 25); //if can read card flash the red led.
+	waterLH = waterLM = waterLL = waterLWL = waterMBtemp = watererBTtemp = lightSensor = batteryVoltage = coopAirTemp = utilitiesAirTemp = 0;
+	lcd.begin(16, 2); //Initalize the LCD with 16 col and 2 rows
+	//analog pin setup
+	lightSensor = analogRead(lightSensorPin); waterLM = analogRead(waterLMPin);
+	waterLH = analogRead(waterLHPin); waterMBtemp = analogRead(waterMBtempPin);
+	watererBTtemp = analogRead(watererBTtempPin); batteryVoltage = analogRead(batteryVoltagePin);
+	coopAirTemp = analogRead(coopAirTempPin); utilitiesAirTemp = analogRead(utilitiesAirTempPin);	
+	//digital pin setup
+	pinMode(waterLLPin,INPUT); pinMode(waterLWLPin, INPUT);
+	Serial.begin(19200);
+	while (!Serial);
+ // flashLED(red, 6, 25);
  // flashLED(yellow, 4, 50);
  // flashLED(green, 5, 75);
  
@@ -48,6 +52,9 @@ void setup() {  // put your setup code here, to run once:
 
 void loop() { // put your main code here, to run repeatedly:
 	//get light sensor value
+	lightSensor = analogRead(lightSensorPin);
+	// print out the value you read:
+	Serial.println(lightSensor);
 	//figure out math for when to turn on lights 
 	//check arduino internal time for refrence
 	//get water bucket levels
@@ -58,7 +65,7 @@ void loop() { // put your main code here, to run repeatedly:
 	//Display info on LCD
 
 }
-//**********************************Functions****************************************//
+//**********************************Functions********************************//
 void flashLED(int colorPin, int times, int timeDelay)
 { //flashLED(red, 6, 25)
   byte hold = 0;
@@ -70,6 +77,9 @@ void flashLED(int colorPin, int times, int timeDelay)
       hold++;
     }
 }
+//---------------------------Time till Daylight--------------------------------
+
 //      Serial.print("Writing to test.txt...");
 //      LogFile.print(tempC); LogFile.print(","); 
 //      Serial.println("done.");
+//		PWM: 3, 5, 6, 9, 10, and 11. Provide 8-bit PWM output with the analogWrite() function.
