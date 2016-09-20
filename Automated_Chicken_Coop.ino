94d2ce218164fa9a68f44c8876fc725bc97329ff
Seth Bowen <sethmath@gmail.com>
2016-09-20 13:48:27 -0400

    Update Automated_Chicken_Coop.ino
    Added Variables

--- Automated_Chicken_Coop.ino
+++ Automated_Chicken_Coop.ino
@@ -1,10 +1,20 @@
 //Static Variables
 byte red = 2, yellow = 3, green = 4; 
-byte sensorPin = A0;
-int sensorValue = 0;
+byte waterLLPin = 5;       //Water Level Low
+byte waterLMPin = 6;      //Water Level Mid
+byte waterLHPin = 7;      //Water Level High
+byte waterLWLPin = 8;     //Water Level waterer Low
+byte waterMBtempPin = A0;   //Water Main bucket Temp
+byte watererBTtempPin = A1;   //Waterer Bucket Temp
+byte lightSensorPin = A2;   //Light Sensor for determining sun set and rising.
+byte batteryVoltagePin = A3;  //Battery Voltage 
+byte coopAirTempPin = A4;     //Air Temp in coop
+byte utiltiesAirTempPin = A5;   //Air Temp in utilties enclosure 
+int waterLH, waterLM, waterLL, waterLWL, waterMBtemp, watererBTtemp, lightSensor, batteryVoltage, coopAirTemp, utiltiesAirTemp;
 
 void setup() {
   // put your setup code here, to run once:
+  waterLH = waterLM = waterLL = waterLWL = waterMBtemp = watererBTtemp = lightSensor = batteryVoltage = coopAirTemp = utiltiesAirTemp = 0;
   pinMode(red, OUTPUT); pinMode(yellow, OUTPUT);  pinMode(green, OUTPUT);
   Serial.begin(19200);
   while (!Serial)
@@ -16,22 +26,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 //  flashLED(green, 5, 30);
-  sensorValue = analogRead(sensorValue);  
-  Serial.println(sensorValue); 
-  if(sensorValue > 750) {
-    flashLED(green, 1, 4);
-  }
-  else if(sensorValue <= 750 && sensorValue >= 250){
+//  sensorValue = analogRead(sensorValue);  
+//  Serial.println(sensorValue); 
+//  if(sensorValue > 750) {
+//    flashLED(green, 1, 4);
+//  }
+//  else if(sensorValue <= 750 && sensorValue >= 250){
     flashLED(yellow, 1, 4);
-  }
-  else {
-     flashLED(red, 1, 4);
+ // }
+//  else {
+//     flashLED(red, 1, 4);
      
-  }
+ // }
 }
 //**********************************Functions****************************************//
 void flashLED(int colorPin, int times, int timeDelay)
-{
+{ //flashLED(red, 6, 25)
   byte hold = 0;
     while(hold < times){
       digitalWrite(colorPin, HIGH);
@@ -44,3 +54,4 @@ void flashLED(int colorPin, int times, int timeDelay)
 //      Serial.print("Writing to test.txt...");
 //      LogFile.print(tempC); LogFile.print(","); 
 //      Serial.println("done.");
+//d pins 2-10 A pin 0-3,6-7
